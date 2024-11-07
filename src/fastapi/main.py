6b03b2a6a5c0f1e426a3548d8174646de6743a6a
JW <jonasz.witek@pm.me>
2024-11-07 11:45:43 +0100

    background service

--- src/fastapi/main.py
+++ src/fastapi/main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import asyncio
 
-app = FastAPI()
+async def print_task(s): 
+    while True:
+        print('Hello from background')
+        await asyncio.sleep(s)
+             
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Run at startup
+    asyncio.create_task(print_task(5))
+    yield
+    # Run on shutdown (if required)
+    print('Shutting down...')
+    
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
     return { "message": "Hello 2"}
 
 @app.get("/posts")
-def get_posts():
+async def get_posts():
     return { "data": "posts"}
